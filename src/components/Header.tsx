5d9059ae5c0f9726adeb548f03a403bb1423ae55
Luã Álvaro <luan.alc@hotmail.com>
2021-08-29 16:31:45 -0300

    feat: menu works

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Flex, Image } from '@chakra-ui/react'
+import { Flex, Image, Box, Text } from '@chakra-ui/react'
 import { useContext } from 'react'
 import { FaBars, FaTimes } from 'react-icons/fa'
 import Logo from '../assets/logo.svg'
@@ -10,28 +10,55 @@ export default function Header() {
     const { categories, menuIsOpen, toggleMenu } = useContext(DrinkableContext)
 
     return (
-        <Flex
-            height="60px"
-            background="#fff"
-            px="20px"
-            justify="space-between"
-            align="center"
-        >
-            <Link to="/">
-                <Image src={Logo} alt="Logo" width="180px" />
-            </Link>
+        <>
+            <Flex
+                height="60px"
+                background="#fff"
+                px="20px"
+                justify="space-between"
+                align="center"
+            >
+                <Link to="/">
+                    <Image src={Logo} alt="Logo" width="180px" />
+                </Link>
 
-            {menuIsOpen
-                ? <FaTimes
-                    fontSize="20px"
-                    onClick={toggleMenu}
-                />
-                : <FaBars
-                    fontSize="20px"
-                    onClick={toggleMenu}
-                />
-            }
+                {menuIsOpen
+                    ? <FaTimes
+                        fontSize="20px"
+                        onClick={toggleMenu}
+                    />
+                    : <FaBars
+                        fontSize="20px"
+                        onClick={toggleMenu}
+                    />
+                }
+            </Flex>
+            <Box
+                display={menuIsOpen ? 'block' : 'none'}
+                background="#fff"
+            >
+                <Text fontWeight="500" fontSize="22px" ml="20px">Categorias</Text>
+                <Box
+                    py="25px"
+                >
+                    {categories.map((category, index) => {
 
-        </Flex>
+                        let linkCategory = category.replaceAll(' / ', '_')
+                        linkCategory = linkCategory.replaceAll('/', '_')
+                        linkCategory = linkCategory.replaceAll(' ', '_')
+
+                        return (
+                            <Text
+                                key={index}
+                                textAlign="center"
+                                fontSize="18px"
+                            >
+                                <Link to={`c=${linkCategory}`}>{category}</Link>
+                            </Text>
+                        )
+                    })}
+                </Box>
+            </Box>
+        </>
     )
 }
